47bde9a1bfdcee3b50aa86b10f0eb219bb93633f
MauRobles4 <roblesbalderramamauricio3amp@gmail.com>
2022-02-07 01:28:12 -0700

    directo, bresenham y dda

--- Linea/dda.js
+++ Linea/dda.js
@@ -1,123 +1,49 @@
-var clicks = 0;
-var x1 = +x1, x2 = +x2, y1 = +y1, y2 = +y2, xf = +xf, xi = +xi;
-var aux = 0.0;
-function dda(event) {
-    // var inicio=0;
-    clicks = clicks + 1;
-    var lienzo = document.getElementById("canvas").getContext("2d");
-    // alert(clicks);
-    if (clicks == 1) {
+function dda(x1, y1, x2, y2, lienzo){ 
+    var dx = Math.abs(x1 - x2);
+    var dy = Math.abs(y1 - y2);
+    var antx, anty;
 
-        x1 = event.clientX;
-        y1 = event.clientY;
-        // x1=1;
-        // y1=2;
-        var coor = "X: " + x1 + ", Y: " + y1;
-        // clicks = clicks + 1;
-        document.getElementById("demo").innerHTML = coor;
-
-    } else if (clicks == 2) {
-        // alert("Entra al 2do click");
-        x2 = event.clientX;
-        y2 = event.clientY;
-        // x2=4;
-        // y2=4;
-        var coor = "X: " + x2 + ", Y: " + y2;
-        // clicks = clicks + 1;
-        document.getElementById("demo").innerHTML = coor;
-    } else {
-        // alert(clicks);
-        alert("Solo se necesitan dos coordenadas");
-    }
-
-    // alert(x1 + " " + y1 + " " + x2 + " " + y2);
-    // CANVAS
-    // var lienzo =  document.getElementById("canvas");
-    // var x1 = 1, x2 = 30, y1 = 2, y2 = 40;
-
-    var m = (y2 - y1) / (x2 - x1);
-    var dify = Math.abs(y2 - y1);
-    var difx = Math.abs(x2 - x1);
-    xi = x1;
-    xf = x2;
-    // alert("Este es el valor de m= "+ m);
-    // var b = y1 - (m * x1);
-    // var context = lienzo.getContext("2d");
-    lienzo.fillStyle = "gray";
-    // context.fillStyle = "gray";
-    // var y = 0;
-    // var x = 0;
-    // alert(x1+ " "+ x2+" "+ y1+" " +y2);
-
-    if (difx > dify) {
-        aux = y1 + m;
-        if (x1 < x2) {
-            for (var i = x1; i <= x2; i++) {
-                // x1++;
-                // if (xi!=xf) {
-                // var y = Math.round(Number(i + m))
-                var y = Math.round(aux);
-                // alert("Suma=" +aux + " y="+ y );
-                aux = aux + m;
-                lienzo.fillRect(i - 4, y - 50, 4, 4);
-
-                // }
-                // context.fillRect(i - 10, y - 10, 4, 4);
-
+    if(dy>dx){
+        var m = (x2-x1)/(y2-y1);
+        if(y1<y2){
+            antx = x1;
+            var x = x1;
+            for (var y=y1; y<=y2; y++){
+                
+                lienzo.fillRect(Math.round(x), y, 4, 4);
+                x = antx + m;
+                antx = x;
             }
-        } else {
-
-            for (var i = x2; i <= x1; i++) {
-                // x1++;
-                // if (xi!=xf) {
-                // var y = Math.round(Number(i + m))
-                var y = Math.round(aux);
-                // alert("Suma=" +aux + " y="+ y );
-                aux = aux + m;
-                lienzo.fillRect(y - 4, i - 50, 4, 4);
-
-                // }
-                // var x = Math.round(Number(m * i + b))
-                // lienzo.fillRect(x-4, i-50, 4, 4);
+        }else{
+            antx = x2;
+            var x = x2;
+            for (var y=y2; y<=y1; y++){
+                
+                lienzo.fillRect(Math.round(x), y, 4, 4);
+                x = antx + m;
+                antx = x;
             }
         }
-
-
-    } else if (difx < dify) {
-        aux = x1 + m;
-        if (y1 < y2) {
-
-            for (var i = y1; i <= y2; i++) {
-                // x1++;
-                // if (xi!=xf) {
-                // var y = Math.round(Number(i + m))
-                var x = Math.round(aux);
-                // alert("Suma=" +aux + " y="+ y );
-                aux = aux + m;
-                lienzo.fillRect(x - 4, i - 50, 4, 4);
-
-                // }
-                // var x = Math.round(Number((i - b) / m))
-                // lienzo.fillRect(x-4, i-50, 4, 4);
+    }else{
+        var m = (y2-y1)/(x2-x1);
+        if(x1<x2){
+            anty = y1;
+            var y = y1;
+            for (var x=x1; x<=x2; x++){
+                
+                lienzo.fillRect(x,Math.round(y), 4, 4);
+                y = anty + m;
+                anty = y;
             }
-        } else {
-            for (var i = y2; i <= y1; i++) {
-                var x = Math.round(aux);
-                // alert("Suma=" +aux + " y="+ y );
-                aux = aux + m;
-                lienzo.fillRect(x - 4, i - 50, 4, 4);
-                // var x = Math.round(Number((i - b) / m))
-                // lienzo.fillRect(x-4, i-50, 4, 4);
+        }else{
+            anty = y2;
+            var y = y2;
+            for (var x=x2; x<=x1; x++){
+                
+                lienzo.fillRect(x,Math.round(y), 4, 4);
+                y = anty + m;
+                anty = y;
             }
         }
-
-    }
-    else {
-        for (var i = x1; i <= x2; i++) {
-            var y = Math.round(Number(m * i + b))
-            lienzo.fillRect(i - 4, y - 50, 4, 4);
-        }
-
-    }
-
+    }       
 }
--- Linea/directo.js
+++ Linea/directo.js
@@ -1,85 +1,51 @@
-var clicks=0;
-var x1 = +x1, x2 = +x2, y1 = +y1, y2 = +y2;
-function showCoords(event) {
-    // var inicio=0;
-    clicks=clicks+1;
-    var lienzo = document.getElementById("canvas").getContext("2d");
-    // alert(clicks);
-    if (clicks == 1) {
-        // alert("Entra");
-        x1 = event.clientX;
-        y1 = event.clientY;
-        var coor = "X: " + x1 + ", Y: " + y1;
-        // clicks = clicks + 1;
-        document.getElementById("demo").innerHTML = coor;
-
-    } else if (clicks == 2) {
-        // alert("Entra al 2do click");
-        x2 = event.clientX;
-        y2 = event.clientY;
-        var coor = "X: " + x2 + ", Y: " + y2;
-        // clicks = clicks + 1;
-        document.getElementById("demo").innerHTML = coor;
-    } else {
-        // alert(clicks);
-        alert("Solo se necesitan dos coordenadas");
-    }
-
-    // alert(x1 + " " + y1 + " " + x2 + " " + y2);
-    // CANVAS
-    // var lienzo =  document.getElementById("canvas");
-    // var x1 = 1, x2 = 30, y1 = 2, y2 = 40;
-
+function directo(x1, y1, x2, y2, lienzo) {
     var m = (y2 - y1) / (x2 - x1);
-    var dify = Math.abs(y2 - y1);
-    var difx = Math.abs(x2 - x1);
-    // alert("Este es el valor de m= "+ m);
     var b = y1 - (m * x1);
-    // var context = lienzo.getContext("2d");
-    lienzo.fillStyle = "gray";    
-    // context.fillStyle = "gray";
-    // var y = 0;
-    // var x = 0;
-    // alert(x1+ " "+ x2+" "+ y1+" " +y2);
-    if (difx > dify) {
-        if (x1 < x2) {
-            for (var i = x1; i <= x2; i++) {
-                // alert("Entra");
-                var y = Math.round(Number(m * i + b))
-                lienzo.fillRect(i-4 , y-50, 4, 4);
-                // context.fillRect(i - 10, y - 10, 4, 4);
+    var dx = Math.abs(x1 - x2);
+    var dy = Math.abs(y1 - y2);
 
-            }
-        } else {
-
-            for (var i = x2; i <= x1; i++) {
-                var x = Math.round(Number(m * i + b))
-                lienzo.fillRect(x-4, i-50, 4, 4);
-            }
-        }
-
-
-    } else if (difx < dify) {
+    if (dx == 0) {
         if (y1 < y2) {
-
-            for (var i = y1; i <= y2; i++) {
-                var x = Math.round(Number((i - b) / m))
-                lienzo.fillRect(x-4, i-50, 4, 4);
+            for (var y = y1; y <= y2; y++) {
+                
+                lienzo.fillRect(x1, y, 4, 4);
             }
         } else {
-            for (var i = y2; i <= y1; i++) {
-                var x = Math.round(Number((i - b) / m))
-                lienzo.fillRect(x-4, i-50, 4, 4);
+            for (var y = y2; y <= y1; y++) {
+                
+                lienzo.fillRect(x1, y, 4, 4);
             }
         }
+    } else {
+        if (dy > dx) {
+            if (y1 < y2) {
+                for (var y = y1; y <= y2; y++) {
+                    var x = (y - b) / m;
+                    
+                    lienzo.fillRect(Math.round(x), y, 4, 4);
+                }
+            } else {
+                for (var y = y2; y <= y1; y++) {
+                    var x = (y - b) / m;
+                    
+                    lienzo.fillRect(Math.round(x), y, 4, 4);
 
-    }
-    else {
-        for (var i = x1; i <= x2; i++) {
-            var y = Math.round(Number(m * i + b))
-            lienzo.fillRect(i-4, y -50, 4, 4);
+                }
+            }
+        } else {
+            if (x1 < x2) {
+                for (var x = x1; x <= x2; x++) {
+                    var y = (m * x) + b;
+                    
+                    lienzo.fillRect(Math.round(x), y, 4, 4);
+                }
+            } else {
+                for (var x = x2; x <= x1; x++) {
+                    var y = (m * x) + b;
+                    
+                    lienzo.fillRect(Math.round(x), y, 4, 4);
+                }
+            }
         }
-
     }
-
 }
